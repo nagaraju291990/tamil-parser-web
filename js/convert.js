d85378560b3fc055a63ed87bc347c4462f7d618a
nagaraju291990 <vpp.nagaraju1234@gmail.com>
2023-08-07 17:07:17 +0530

    new bootstrap

--- js/convert.js
+++ js/convert.js
@@ -13,6 +13,8 @@ $(document).ready(function(){
 		width: "100%",
 		height: "100%"
 	});
+	const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+	const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
 
 function startRead(evt) {
