bf0df82f49ceaad796b6a76655c24b9ba27daba3
parameshkrishnaa <parameshkrishnaa@gmail.com>
2023-08-21 14:51:37 +0530

    Revert "tmp files direcotry created"
    This reverts commit 4134b725cbd913c4f892a797da0e5dbdf7d144b3.

--- scripts/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const pos_tag_command1 = 'python3 postagger/Method1_WordPOS/method1.py';
-const pos_tag_command2 = 'python3 postagger/Method2_SubWordPOS/method2.py';
-const pos_tag_command3 = 'sh postagger/postagger.sh /var/www/html/tamil-parser/';
-
-const convertor_command = 'perl /home/user/Tamil-Parser/tools/convertor-indic-1.4.7/convertor_indic.pl';
-?>
